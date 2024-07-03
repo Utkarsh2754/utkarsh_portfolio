68f655a34e755e0b76e603049914c2f3ddb86793
Utkarsh2754 <143296225+Utkarsh2754@users.noreply.github.com>
2024-07-04 02:57:52 +0530

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,15 +1,39 @@
+const navMenu = document.getElementById('nav_menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
+
+
+
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove('show-menu');
+        console.log("hhii");
+    });
+}
+
 const navlink = document.querySelectorAll('.nav__link');
 
-function activelink()
-{
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+
+navlink.forEach((n) => n.addEventListener('click', linkAction));
+
+function activelink() {
     navlink.forEach((a) => a.classList.remove('active-link'));
     this.classList.add('active-link');
 }
 
 navlink.forEach((a) => a.addEventListener('click', activelink));
 
-function scrollHeader()
-{
+function scrollHeader() {
     const header = document.getElementById('header');
 
     if (this.scrollY >= 50) header.classList.add('scroll-header');
